0891197120c76bf616727b7c838ebab29cc3438a
Covalent <covalent5@gmail.com>
2023-12-27 18:34:03 +0200

    Added the test function.

--- DataCache/Navigation/ContentView.swift
+++ DataCache/Navigation/ContentView.swift
@@ -1,9 +1,9 @@
 /*
-See the LICENSE.txt file for this sample’s licensing information.
-
-Abstract:
-The app's top level navigation split view.
-*/
+ See the LICENSE.txt file for this sample’s licensing information.
+ 
+ Abstract:
+ The app's top level navigation split view.
+ */
 
 import SwiftUI
 import SwiftData
@@ -13,9 +13,9 @@ struct ContentView: View {
     @Environment(ViewModel.self) private var viewModel
     @Environment(\.modelContext) private var modelContext
     @Environment(\.scenePhase) private var scenePhase
-
+    
     @Query private var quakes: [Quake]
-
+    
     /// The identifier of the selected earthquake.
     ///
     /// The list controls this value through a binding, and most of the app's
@@ -24,16 +24,16 @@ struct ContentView: View {
     /// but using separate values enables the app to detect when someone taps
     /// on the map so that it can scroll the list to match.
     @State private var selectedId: Quake.ID? = nil
-
+    
     /// The identifier of the earthquake that the map highlights.
     ///
     /// This distinct map selection state enables the app to use
     /// changes in map selection to drive scroll changes in the list.
     @State private var selectedIdMap: Quake.ID? = nil
-
+    
     var body: some View {
         @Bindable var viewModel = viewModel
-
+        
         NavigationSplitView {
             QuakeList(
                 selectedId: $selectedId,
@@ -47,12 +47,18 @@ struct ContentView: View {
             .toolbar {
                 // In iOS, the refreshable modifier provides pull-to-refresh.
                 // In macOS, add a refresh button to the toolbar instead.
-                #if os(macOS)
+#if os(macOS)
                 RefreshButton()
-                #endif
+#endif
                 
                 DeleteButton(selectedId: $selectedId)
                 SortButton()
+                Button {
+                    addQuakes(count: 10000, to: modelContext)
+                } label: {
+                    Label("Add", systemImage: "plus").labelStyle(.iconOnly)
+                }
+                
             }
             
             // This modifier creates a pull-to-refresh in iOS, but also sets
@@ -64,25 +70,57 @@ struct ContentView: View {
             }
             .navigationTitle("Earthquakes")
         } detail: {
-            MapView(
-                selectedId: $selectedId,
-                selectedIdMap: $selectedIdMap,
-                searchDate: viewModel.searchDate,
-                searchText: viewModel.searchText
-            )
-            #if os(macOS)
+            //            MapView(
+            //                selectedId: $selectedId,
+            //                selectedIdMap: $selectedIdMap,
+            //                searchDate: viewModel.searchDate,
+            //                searchText: viewModel.searchText
+            //            )
+            Group {
+                if let selectedQuake = quakes.first(where: { $0.id == selectedId }) {
+                    Text("Magnitude: \(selectedQuake.magnitudeString)")
+                        .font(.title)
+                } else {
+                    Text("Select an Earthquake")
+                }
+            }
+#if os(macOS)
             .navigationTitle(quakes[selectedId]?.location.name ?? "Earthquakes")
             .navigationSubtitle(quakes[selectedId]?.fullDate ?? "")
-            #else
+#else
             .navigationTitle(quakes[selectedId]?.location.name ?? "")
             .navigationBarTitleDisplayMode(.inline)
-            #endif
+#endif
         }
         .onChange(of: scenePhase) { _, scenePhase in
             if scenePhase == .active {
                 viewModel.update(modelContext: modelContext)
             }
         }
+    }
+}
+
+extension ContentView {
+    func addQuakes(count: Int, to modelContext: ModelContext) {
+        for _ in 0..<count {
+            let quake = generateRandomQuake()
+            try? modelContext.insert(quake)
+        }
+        viewModel.update(modelContext: modelContext)
+    }
+    
+    /// Generates a random earthquake.
+    /// - Returns: A `Quake` object with random properties.
+    private func generateRandomQuake() -> Quake {
+        // Generate random data for the quake
+        let code = UUID().uuidString
+        let magnitude = Double.random(in: 0...10)
+        let time = Date()// Random date within the past year
+        let name = "Random Location"
+        let longitude = Double.random(in: -180...180)
+        let latitude = Double.random(in: -90...90)
+        
+        return Quake(code: code, magnitude: magnitude, time: time, name: name, longitude: longitude, latitude: latitude)
     }
 }
 
